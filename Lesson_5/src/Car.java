fa226f98a88b24e531b3190c0d445ad3b7004147
Alexander Ermakov <ermakov.a.a@mail.ru>
2020-03-16 22:27:49 +0300

    Задание к 5 уроку

--- Lesson_5/src/Car.java
+++ Lesson_5/src/Car.java
@@ -1,34 +1,48 @@
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
+
     static {
         CARS_COUNT = 0;
     }
+
     private Race race;
     private int speed;
     private String name;
-    public String getName() {
-        return name;
-    }
-    public int getSpeed() {
-        return speed;
-    }
-    public Car(Race race, int speed) {
+
+    public Car(Race race, int speed, CyclicBarrier cb) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
-            Thread.sleep(500 + (int)(Math.random() * 800));
+            Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+
+            MainClass.cdl.countDown();
+            MainClass.car_cb.await();
+
+            for (int i = 0; i < race.getStages().size(); i++) {
+                race.getStages().get(i).go(this);
+            }
+
+            MainClass.cdl.countDown();
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- Lesson_5/src/Race.java
+++ Lesson_5/src/Race.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Race {
-    private ArrayList<Stage> stages;
-    public ArrayList<Stage> getStages() { return stages; }
+    private static ArrayList<Stage> stages;
+
     public Race(Stage... stages) {
         this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+
+    public ArrayList<Stage> getStages() {
+        return stages;
     }
 }
